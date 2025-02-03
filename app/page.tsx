60c64680c17853daadab75230d39a12ef8dad73c
Krati97 <kabara.krati@gmail.com>
2025-02-03 12:22:16 -0800

    updating import of grid to dynamic import

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import Grid from "@/components/Grid";
+"use client";
 import Hero from "@/components/Hero";
 import PreviousExperiences from "@/components/PreviousExperiences";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { navItems } from "@/data";
-// import { FaHome } from "react-icons/fa";
-
+import dynamic from "next/dynamic";
+const Grid = dynamic(() => import("../components/Grid"), {
+  ssr: false,
+  });
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
