fd203e10a61c4a807248e9f73707c2a52b3ecf59
Krati97 <kabara.krati@gmail.com>
2025-02-08 20:46:11 -0800

    Alignment issue improved

--- components/ui/TechStack.tsx
+++ components/ui/TechStack.tsx
@@ -10,7 +10,7 @@ const TechStack = () => {
       <h1 className="heading">
         My <span className="text-purple">Skills</span>
       </h1>
-      <div className="w-full mt-5 mb-10 items-center justify-center max-lg:mt-5 border border-red  rounded-lg ">
+      <div className="w-full mt-10 mb-10 items-center justify-center max-lg:mt-5 border border-red  rounded-lg ">
         <motion.div className="flex flex-wrap gap-5 mt-0 justify-center items-center mx-auto ">
           {skills.map((el: any) => (
             <motion.div
